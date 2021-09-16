325f14a4aa7ee2c26c0241f177306e1f2d9960b1
DanielAdeyemi <adeyemidany@gmail.com>
2021-09-15 21:25:21 -0700

    intro to data structures in Python

--- hello.py
+++ hello.py
@@ -8,3 +8,8 @@ elif age < 21:
     print(f"No drinks for you, {name}")
 else:
     print(f"Hello, {name}. Since you are {age}, what drink do you want?")
+print(f"Your name starts from {name[0]}")
+
+# info = (name, age)  # tuple
+# info[0] = 'Greg'
+# print(info[0])
