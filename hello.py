26f346c60007b23562875f8fe74d3971d37ddc2a
DanielAdeyemi <adeyemidany@gmail.com>
2021-09-15 21:40:15 -0700

    work with python list

--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
-name = input("Name: ")
+names = []  # empty list of names
+ages = set()
 
-age = int(input("Number: "))
+for i in range(3):
+    name = input("Name: ")
+    names.append(name)  # add element to the end of the list
 
-if age < 0:
-    print("Not a real age")
-elif age < 21:
-    print(f"No drinks for you, {name}")
-else:
-    print(f"Hello, {name}. Since you are {age}, what drink do you want?")
-print(f"Your name starts from {name[0]}")
+    age = int(input("Age: "))
+
+    if age < 0:
+        print("Not a real age")
+    elif age < 21:
+        print(f"No drinks for you, {name}")
+    else:
+        print(f"Hello, {name}. Since you are {age}, what drink do you want?")
+
+names.sort()
+print(names)
 
 # info = (name, age)  # tuple
 # info[0] = 'Greg'
