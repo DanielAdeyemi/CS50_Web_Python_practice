06f064112f2307bb5efc26585a3d60a59c732a17
DanielAdeyemi <adeyemidany@gmail.com>
2021-09-15 21:14:27 -0700

    conditions and int cast

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 name = input("Name: ")
 
-print(f"Hello, {name}")  # formatted string
+age = int(input("Number: "))
+
+if age < 0:
+    print("Not a real age")
+elif age < 21:
+    print(f"No drinks for you, {name}")
+else:
+    print(f"Hello, {name}. Since you are {age}, what drink do you want?")
