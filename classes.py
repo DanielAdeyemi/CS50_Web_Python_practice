ca5098049b4e79fd96cd611062f77439cf174b6c
DanielAdeyemi <adeyemidany@gmail.com>
2021-09-25 21:34:45 -0700

    initiate new val with point

--- classes.py
+++ classes.py
@@ -2,3 +2,8 @@ class Point():
     def __init__(self, x, y):  # create an object, self references object itself
         self.x = x
         self.y = y
+
+
+p = Point(2, 8)  # initiate new Point and print the data from inside
+print(p.x)
+print(p.y)
