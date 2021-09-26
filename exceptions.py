78f6138de316119d1dd88b88d310f3683b2f39d2
DanielAdeyemi <adeyemidany@gmail.com>
2021-09-25 22:09:05 -0700

    try/except block

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+import sys
+
+x = int(input("x: "))
+y = int(input("y: "))
+
+try:
+    result = x/y
+except ZeroDivisionError:
+    print("Error: Cannot divide by 0.")
+    sys.exit(1)
+
+print(f"x/y = {result}")
